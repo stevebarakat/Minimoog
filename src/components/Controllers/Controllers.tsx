d72af136691240a9d1c188189401ced05e45c0a1
stevebarakat <stevebarakat@gmail.com>
2025-07-22 15:21:56 -0400

    ci

--- src/components/Controllers/Controllers.tsx
+++ src/components/Controllers/Controllers.tsx
@@ -7,6 +7,8 @@ import Section from "../Section";
 import Column from "../Column";
 import Row from "../Row";
 import { useSynthStore } from "@/store/synthStore";
+import SidePanel from "../SidePanel";
+import { useIsMobile } from "@/hooks/useMediaQuery";
 
 function Controllers() {
   const {
@@ -16,6 +18,7 @@ function Controllers() {
     setNoiseLfoSwitch,
     isDisabled,
   } = useSynthStore();
+  const isMobile = useIsMobile();
 
   return (
     <Section
@@ -26,34 +29,37 @@ function Controllers() {
         borderRadius: "0 0 0 0.5rem",
       }}
     >
-      <Column gap="var(--spacing-lg)">
-        <Tune />
-        <Row gap="var(--spacing-xl)">
-          <Glide />
-          <ModulationMix />
-        </Row>
-        <Row
-          justify="space-around"
-          style={{ marginBottom: "var(--spacing-md)" }}
-        >
-          <RockerSwitch
-            checked={osc3FilterEgSwitch}
-            onCheckedChange={setOsc3FilterEgSwitch}
-            label="Send to mod 1"
-            bottomLabelLeft="Osc. 3"
-            bottomLabelRight="Filter Eg"
-            disabled={isDisabled}
-          />
-          <RockerSwitch
-            checked={noiseLfoSwitch}
-            onCheckedChange={setNoiseLfoSwitch}
-            label="Send to mod 2"
-            bottomLabelLeft="Noise"
-            bottomLabelRight="LFO"
-            disabled={isDisabled}
-          />
-        </Row>
-      </Column>
+      <Row>
+        {isMobile && <SidePanel />}
+        <Column gap="var(--spacing-lg)">
+          <Tune />
+          <Row gap="var(--spacing-xl)">
+            <Glide />
+            <ModulationMix />
+          </Row>
+          <Row
+            justify="space-around"
+            style={{ marginBottom: "var(--spacing-md)" }}
+          >
+            <RockerSwitch
+              checked={osc3FilterEgSwitch}
+              onCheckedChange={setOsc3FilterEgSwitch}
+              label="Send to mod 1"
+              bottomLabelLeft="Osc. 3"
+              bottomLabelRight="Filter Eg"
+              disabled={isDisabled}
+            />
+            <RockerSwitch
+              checked={noiseLfoSwitch}
+              onCheckedChange={setNoiseLfoSwitch}
+              label="Send to mod 2"
+              bottomLabelLeft="Noise"
+              bottomLabelRight="LFO"
+              disabled={isDisabled}
+            />
+          </Row>
+        </Column>
+      </Row>
       <Title>Controllers</Title>
     </Section>
   );
--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -7,12 +7,15 @@ import LfoRate from "../LfoRate";
 import Column from "../Column";
 import Row from "../Row";
 import * as Tooltip from "@radix-ui/react-tooltip";
-import Screw from "../Screw/Screw";
+import Screw from "../Screw";
+import { useIsMobile } from "@/hooks/useMediaQuery";
 
 function SidePanel() {
+  const isMobile = useIsMobile();
+
   return (
     <Tooltip.Provider>
-      <Column className={styles.sidePanel}>
+      <Column className={isMobile ? styles.mobileSidePanel : styles.sidePanel}>
         <Row gap="var(--spacing-md)">
           <LfoRate />
           <div className={styles.switches}>
@@ -32,9 +35,17 @@ function SidePanel() {
           </Column>
           <ModulationWheel />
         </Row>
-        <Row justify="flex-end" gap="5rem" style={{ top: "14px" }}>
-          <Screw />
-          <Screw />
+        <Row
+          justify="flex-end"
+          gap="5rem"
+          style={
+            isMobile ? { top: "14px", marginBottom: "1rem" } : { top: "14px" }
+          }
+        >
+          <>
+            <Screw hidden={isMobile} />
+            <Screw hidden={isMobile} />
+          </>
         </Row>
       </Column>
     </Tooltip.Provider>
