78caff2a39992f3c79bd78d57dd21f9cf34c71c7
stevebarakat <stevebarakat@gmail.com>
2025-08-01 12:41:33 -0400

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
+    // react({
+    //   babel: {
+    //     plugins: ["babel-plugin-react-compiler"],
+    //   },
+    // }),
     tsconfigPaths(),
   ],
   optimizeDeps: {
